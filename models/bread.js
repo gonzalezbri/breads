f046c195487d3497379d89a3c79a14d41f8e9776
gonzalezbri <4717bg@gmail.com>
2023-04-25 19:08:25 -0700

    added everything else

--- models/bread.js
+++ models/bread.js
@@ -7,7 +7,7 @@ module.exports = [
     {
       name: 'French',
       hasGluten: true,
-       image: 'https://images.unsplash.com/photo-1534620808146-d33bb39128b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
+      image: 'https://images.unsplash.com/photo-1534620808146-d33bb39128b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
     },
     {
       name: 'Gluten-Free',
@@ -18,6 +18,11 @@ module.exports = [
       name: 'Pumpernickel',
       hasGluten: true,
       image: 'https://images.unsplash.com/photo-1586444248902-2f64eddc13df?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
+    },
+    {
+      name:'potato bread',
+      hasGluten: false,
+      image:'https://www.breadsmith.com/wp-content/uploads/2015/09/Potato-Bread.png'
     }
   ]
   
--- server.js
+++ server.js
@@ -6,6 +6,13 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 
+//MIDDLEWARE
+app.set('views',__dirname + '/views');
+app.set('view engine','jsx');
+app.engine('jsx',require('express-react-views').createEngine());
+app.use(express.static('public'))
+
+
 // ROUTES
 app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads!')
@@ -18,3 +25,8 @@ app.listen(PORT, () => {
 // Breads
 const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads',breadsController)
+
+// 404 Page
+app.get('*', (req, res) => {
+    res.send('404')
+})
